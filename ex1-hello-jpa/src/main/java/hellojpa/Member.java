89bcaf717317877206fb87151b7723d97fe1a805
kmindev <kangkm0324@gmail.com>
2024-12-12 15:02:48 +0900

    @Entity, @Table 알아보기

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,14 +2,16 @@ package hellojpa;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+@Table(name = "MBR") // 엔티티에 매팡할 테이블 지정
+@Entity // Jpa에서 관리할 엔티티
 public class Member {
     @Id
     private Long id;
     private String name;
 
-    public Member() { // jpa는 기본 생성자가 필요함(접근제한자는 아무거나)
+    public Member() { // jpa는 기본 생성자가 필요함(Public, Protected)
     }
 
     public Member(Long id, String name) {
