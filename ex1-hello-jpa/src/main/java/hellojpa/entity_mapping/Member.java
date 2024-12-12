ee1ec3b24494c96271882d39bfaae028ee5eacc5
kmindev <kangkm0324@gmail.com>
2024-12-12 15:22:55 +0900

    스키마 자동 생성(DDL) 옵션

--- ex1-hello-jpa/src/main/java/hellojpa/entity_mapping/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/entity_mapping/Member.java
@@ -1,15 +1,19 @@
-package hellojpa;
+package hellojpa.entity_mapping;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Table(name = "MBR") // 엔티티에 매팡할 테이블 지정
+//@Table(name = "MBR") // 엔티티에 매팡할 테이블 지정
 @Entity // Jpa에서 관리할 엔티티
 public class Member {
     @Id
     private Long id;
+
+    @Column(unique = true, length = 10) // 제약 조건(DDL 자동 생성에만 적용, JPA 실행로직에는 영향을 주지 않음)
     private String name;
+    private int age;
 
     public Member() { // jpa는 기본 생성자가 필요함(Public, Protected)
     }
