bb4e1710745ca32667f04ab024c388bd0ffbaa04
kmindev <kangkm0324@gmail.com>
2024-12-16 16:35:26 +0900

    Entity 연관관계 매핑

--- jpashop/src/main/java/jpabook/jpashop/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/Order.java
@@ -2,6 +2,8 @@ package jpabook.jpashop;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Table(name = "orders")
 @Entity
@@ -12,12 +14,22 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @Column(name = "member_id")
-    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 
     public Long getId() {
         return id;
@@ -27,12 +39,12 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return memberId;
+    public Member getMember() {
+        return member;
     }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
+    public void setMember(Member member) {
+        this.member = member;
     }
 
     public LocalDateTime getOrderDate() {
@@ -50,5 +62,4 @@ public class Order {
     public void setStatus(OrderStatus status) {
         this.status = status;
     }
-
 }
