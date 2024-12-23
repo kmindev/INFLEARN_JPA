3f3d40850bca3f8103f5e1e7f9ae99d601035208
kmindev <kangkm0324@gmail.com>
2024-12-23 15:36:16 +0900

    고아 객체
    * 고아 객체 제거 기능을 활성화하면 부모 엔티티와 연관관계가 끊어진 자식 엔티티를 자동으로 삭제
    
    * CascadeType.REMOVE처럼 동작

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -96,8 +96,12 @@ public class JpaMain {
             parent.addChild(child2);
 
             em.persist(parent);
-//            em.persist(child1);
-//            em.persist(child2);
+
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent); // casecade remove 옵션, orphanRemoval 활성화시 자식도 함께 삭제
 
             tx.commit();
         } catch (Exception e) {
