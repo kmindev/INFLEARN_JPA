9d4e8de68a2ff828375f9cd61388efeca6b7ed5c
kmindev <kangkm0324@gmail.com>
2025-01-02 15:29:50 +0900

    jpa 값타입 - 공유 객체 사용시 발생하는 문제

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,13 +15,24 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Address address = new Address("city", "street", "100");
 
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "100"));
-            member.setWorkPeriod(new Period());
+            Member member1 = new Member();
+            member1.setUsername("hello");
+            member1.setHomeAddress(address);
+            member1.setWorkPeriod(new Period());
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("hello1");
+            member2.setHomeAddress(address);
+            member2.setWorkPeriod(new Period());
+
+            // 공유 객체를 사용하므로 문제 발생 => member1 을 수정했는데 member2 가 변경됨.
+            // 그러므로 불편 객체를 사용(생성자르로 통해서만 값을 주입하고, setter 메서드 금지)
+            member1.getHomeAddress().setCity("new city");
+
+            em.persist(member1);
+            em.persist(member2);
 
             tx.commit();
         } catch (Exception e) {
