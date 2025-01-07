d3ab07d0f242a9bebee67e9346b8abfeaec94a0e
kmindev <kangkm0324@gmail.com>
2025-01-07 17:34:44 +0900

    JPQL 소개

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -15,66 +16,22 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Address address = new Address("city", "street", "100");
-//
-//            Member member1 = new Member();
-//            member1.setUsername("hello");
-//            member1.setHomeAddress(address);
-//            member1.setWorkPeriod(new Period());
-//
-//            Member member2 = new Member();
-//            member2.setUsername("hello1");
-//            member2.setHomeAddress(address);
-//            member2.setWorkPeriod(new Period());
 
-            // 공유 객체를 사용하므로 문제 발생 => member1 을 수정했는데 member2 가 변경됨.
-            // 그러므로 불편 객체를 사용(생성자르로 통해서만 값을 주입하고, setter 메서드 금지)
-//            member1.getHomeAddress().setCity("new city");
-//
-//            em.persist(member1);
-//            em.persist(member2);
+            // JPQL
+//            String jpql = "select m From Member m where m.username like '%kim%'";
+//            List<Member> resultList = em.createQuery(jpql, Member.class).getResultList();
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("city1", "street", "100000"));
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
+            // Criteria => 동적 쿼리에 유리 (유지보수가 어려우므로 실무에서 사용 x)
+//            CriteriaBuilder cb = em.getCriteriaBuilder();
+//            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+//            Root<Member> m = query.from(Member.class);
+//            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+//            List<Member> resultList = em.createQuery(cq).getResultList();
 
-            member.getAddressHisotry().add(new Address("old1", "street1", "1000"));
-            member.getAddressHisotry().add(new Address("old2", "street2", "2000"));
+            // Native Query ; native 쿼리를 날리기 전에 commit을 함. (jdbc, mybatis를 사용하는 경우 강제로 flush 해야함.)
+            List<Member> resultList = em.createNativeQuery("select member_id, city, street, zipcode, username from member where username = 'kim'", Member.class).getResultList();
 
-            em.persist(member);
 
-            em.flush();
-            em.clear();
-
-            System.out.println("========== start ============");
-            Member findMember = em.find(Member.class, member.getId());
-
-            // 값타입 컬렉션은 지연로딩 전략을 사용한다.
-            for (String food : findMember.getFavoriteFoods()) { // 지연 로딩
-                System.out.println(food);
-            }
-
-            for (Address address : findMember.getAddressHisotry()) { // 지연 로딩
-                System.out.println(address.getCity());
-            }
-
-            Address oldAddress = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("new city", oldAddress.getStreet(), oldAddress.getZipcode())); // 임베디드 타입 업데이트
-
-            // 값 타입 컬렉션 변경(primitive type)
-            // name으로 delete 쿼리
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            // 값 타입 컬렉션 변경(커스텀 객체)
-            // 해당 memeber의 모든 히스토리를 삭제하고, 다시 insert 한다.(delete: 1회, insert: N회)
-            findMember.getAddressHisotry().remove(new Address("old1", "street1", "1000")); // equals&hashcode 구현 필요
-            findMember.getAddressHisotry().add(new Address("new city1", "street1", "1000"));
-
-            System.out.println("========== end ============");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
