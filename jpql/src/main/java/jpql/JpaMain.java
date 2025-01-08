c783f1405bd9e885c006de2d112e099015c565e9
kmindev <kangkm0324@gmail.com>
2025-01-08 14:03:22 +0900

    JPQL - 프로젝션

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -31,7 +31,12 @@ public class JpaMain {
             query2.setParameter("username", "member1");
             Member singleResult1 = query2.getSingleResult();
             System.out.println("singleResult1.getUsername() = " + singleResult1.getUsername());
-            
+
+            // 프로젝션
+            em.createQuery("select m from Member m", Member.class).getResultList(); // 엔티티 프로젝션
+            em.createQuery("select m.team from Member m", Team.class).getResultList(); // 엔티티 프로젝션(join 쿼리) => 별도의 join쿼리를 제공하기에 권장 x
+            em.createQuery("select o.address from Orders o", Address.class).getResultList(); // 임베디드 타입 프로젝션
+            em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList(); // 스칼라 타입 프로젝션
 
 
             tx.commit();
