d4d918021c39a2252f76b110a376b206ef9c5f25
kmindev <kangkm0324@gmail.com>
2025-06-13 15:53:25 +0900

    주문 목록 조회(컬렉션) - JPA에서 DTO 직접 조회(반복문)

--- 실전! 스프링 부트와 JPA 활용/src/main/java/jpabook/jpashop/controller/api/OrderApiController.java
+++ 실전! 스프링 부트와 JPA 활용/src/main/java/jpabook/jpashop/controller/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.entity.OrderItem;
 import jpabook.jpashop.entity.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.query.OrderQueryDto;
+import jpabook.jpashop.repository.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,7 @@ import java.util.List;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
@@ -67,6 +70,13 @@ public class OrderApiController {
         return orderDtos;
     }
 
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+    ) {
+        return orderQueryRepository.findOrderQueryDtos(offset, limit);
+    }
 
     @Data
     static class OrderDto {
